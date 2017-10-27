996601493da56021fc064fa270ba565438ff0ac4
Dong Liu <doliu@microsoft.com>
2017-10-27 13:50:27 +0800

    Script update

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+DOCKER_DISK=/dev/disk/azure/scsi1/lun0
+mkfs.btrfs $DOCKER_DISK
+echo "$DOCKER_DISK /var/lib/docker btrfs rw,relatime 0 2" >> /etc/fstab
+mount -a
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
